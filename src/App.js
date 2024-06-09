375e9269001d3e2b4cd501058dd0e907b773688b
starfart69 <joshuel.simbulan@gmail.com>
2024-06-09 19:45:44 +0800

    initialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const person = { name: "Joshuel", age: "22" } 
+  
+  return (
+    <div className="App">
+      <p>{  }</p>
+    </div>
+  );
+}
+
+export default App;
