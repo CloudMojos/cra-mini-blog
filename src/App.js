072480d110203d0f228b3764b7c157f7408f08d5
starfart69 <joshuel.simbulan@gmail.com>
2024-06-15 20:32:26 +0800

    just uncomment line 12. this is another useless commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="content">
+      {/* <div className="content">
         <Home />
-      </div>
+      </div> */}
     </div>
   );
 }
