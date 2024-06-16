b036474f19e682d2871eb7887b72b8a2b0a2b7ef
starfart69 <joshuel.simbulan@gmail.com>
2024-06-16 22:27:13 +0800

    added title to blogs

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,9 +1,11 @@
 const BlogList = (props) => {
     const blogs = props.blogs
+    const title = props.title
     return ( 
         <div className="blog-list">
+            <h1>{ title }</h1>
             {blogs.map((blog) => (
-                // You always need a key property for the DOM to not bitch up
+                // You always need a key property for the DOM not to bitch up
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Wrtten by: { blog.author }</p>
